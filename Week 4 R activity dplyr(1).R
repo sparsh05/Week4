323cc754a0836761138648e9d5ec898317c5816d
Sparsh05 <42962210+sparsh05@users.noreply.github.com>
2022-04-10 22:59:07 -0400

    Add files via upload

---
+++ Week 4 R activity dplyr(1).R
@@ -0,0 +1,66 @@
+#Week 4: dplyr package
+
+#Task: Write the function to get a dataset from Base R: HairEyeColor
+#and give the dataset a new name of your choice
+df <- data.frame(HairEyeColor)
+
+#See the top rows of the data
+#TASK: Write the function to see the top rows of the data
+head(df)
+
+#Install and call the package dplyr
+#TASK: Write the functions to install and call dplyr
+install.packages('dplyr')
+library(dplyr)
+
+
+#Let's just see the hair and sex columns
+#Task: Write the function to 'select' just the hair and sex columns
+#(hint: use the 'select' function)
+select(df, Hair, Sex)
+
+#Let's name the dataset with just the two columns, Hair and Sex
+#TASK: Write the function to save the two columns as a new dataset
+#and give it a name
+df2 <- data.frame(select(df, Hair, Sex))
+
+#Let's get rid of the Sex column in the new dataset created above
+#TASK: Write the function that deselects the sex column
+#(hint: use the 'select' function to not select a -column)
+select(df2, !Sex)
+
+#Let's rename a column name
+#TASK: Write the function that renames 'sex' to 'gender'
+rename(df2, gender = Sex)
+
+#Let's make a new dataset with the new column name
+#TASK: Write the function that names a new dataset that includes the 'gender' column
+df3 <- data.frame(rename(df2, gender = Sex))
+
+#Let's 'filter' just the males from our dataset
+#TASK: Write the function that includes only rows that are 'male'
+df_male <- filter(df3, gender=='Male')
+
+#Let's 'group' our data by gender
+#TASK: Write the function to group the data by gender (hint: group_by)
+df3 %>%group_by(gender)
+
+#Let's see how many students were examined in the dataset (total the frequency)
+#TASK: replace 'datasetname' with the name of your dataset and get the total
+#After you run it, write the total here:32
+
+#this code was throwing an error which i couldn't get through, hence used nrow(df3) to find number of students examined in the dataset
+total=mutate(df, total=cumsum(Freq))
+
+#Since we have a males dataset, let's make a females dataset
+#TASK: Write the function that includes only rows that are 'female'
+df_female <- filter(df3, gender=='Female')
+
+#And now let's join the males and females
+#TASK: Write the function that joins the male and female rows 
+#(hint: try using 'union' or 'bind_rows')
+bind_rows(df_male, df_female)
+
+#Optional Task: add any of the other functions 
+#you learned about from the dplyr package
+df3 %>% arrange(Hair)
